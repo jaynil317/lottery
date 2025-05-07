ada10f823f077b129c7b9c0dccff32405e7d7352
jaynil019 <jaynilp579@gmail.com>
2025-05-07 13:59:10 +0530

    fourth

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import session
-from passenger_wsgi import application
+# from passenger_wsgi import application
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
 from datetime import datetime, timezone
